5177bf0b136404bc38203339c429158397fdc380
Joonas Govenius <joonas.govenius@gmail.com>
2016-07-24 11:27:26 +0300

    Added a license file.

--- plotbridge/plot.py
+++ plotbridge/plot.py
@@ -1,4 +1,4 @@
-# Joonas Govenius <joonas.govenius@gmail.com>, 2016
+# Copyright (C) 2016 Joonas Govenius <joonas.govenius@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
