8d39f4cdd5b135417d4f119ce16d67bc66f76e77
Joonas Govenius <joonas.govenius@gmail.com>
2016-11-04 10:46:10 +0200

    bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='plotbridge',
-      version='1.0.0',
+      version='1.0.1',
       author='Joonas Govenius',
       py_modules=['plotbridge.py'],
       packages=['plotbridge'],
