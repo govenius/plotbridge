dd4dfa08cc65497ce5aacf9383d31f63776885ae
Joonas Govenius <joonas.govenius@gmail.com>
2022-04-08 21:40:21 +0300

    Move to setup.cfg

--- setup.py
+++ setup.py
@@ -1,10 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(name='plotbridge',
-      version='1.0.4',
-      author='Joonas Govenius',
-      license='MIT',
-      license_files=('LICENSE',),
-      packages=['plotbridge'],
-      include_package_data=True
-      )
+from setuptools import setup
+setup()
